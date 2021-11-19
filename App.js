5c07b0eca5f776fa858a518c7ae74df5b3a5d6b6
Jouni Juntunen <jouni.juntunen@oamk.fi>
2021-11-19 08:53:11 +0200

    Finished example.

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
+import { useFonts } from 'expo-font';
+import StyleSheet from './styles/Styles';
 
 export default function App() {
+
+  // https://docs.expo.dev/versions/latest/sdk/font/
+  const [loaded] = useFonts({
+    Helvetica: require('./assets/fonts/Helvetica.ttf'),
+    Oswald: require('./assets/fonts/Oswald.ttf'),
+    Merriweather: require('./assets/fonts/Merriweather-Regular.ttf'),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={StyleSheet.container}>
+      <Text style={StyleSheet.text}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Image source={require('./assets/images/image.png')} style={{width: 200,height: 200}} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+/* const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  text : {
+    fontFamily: 'Merriweather'
+  }
+}); */
